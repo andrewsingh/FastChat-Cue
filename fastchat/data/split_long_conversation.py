56ef0fb8386212c3cf42baa20a4fd5d1180fe3aa
Andrew Singh <andrew.singh99@gmail.com>
2023-08-24 11:46:51 -0700

    Stop tracking notebooks, update split conversation script

--- fastchat/data/split_long_conversation.py
+++ fastchat/data/split_long_conversation.py
@@ -19,8 +19,6 @@ import pdb
 parser = argparse.ArgumentParser()
 parser.add_argument("--in-file", type=str, required=True)
 parser.add_argument("--out-file", type=str, default="sharegpt_split.json")
-parser.add_argument("--begin", type=int)
-parser.add_argument("--end", type=int)
 parser.add_argument("--model-name-or-path", type=str, required=True)
 parser.add_argument("--max-length", type=int, default=2048)
 parser.add_argument("--use-system-message", action='store_true')
@@ -46,15 +44,29 @@ tokenizer = max_length = None
 
 
 def split_one_sample(sample):
-    # pdb.set_trace()
     max_length_v2 = args.max_length
     if args.use_system_message:
         system_message_len = len(tokenizer(sample['system_message']).input_ids)
         max_length_v2 = max_length_v2 - system_message_len
-        # print(max_length_v2)
+
     tokenized_lens = []
     conversations = sample["conversations"]
-    conversations = conversations[: len(conversations) // 2 * 2]
+
+    if conversations[0]['from'] == 'prospect':
+        conversations = conversations[1:]
+
+    if conversations[-1]['from'] == 'sales rep':
+        conversations = conversations[:-1]
+
+    if len(conversations) < 2:
+        return []
+
+    assert(conversations[0]['from'] == 'sales rep')
+    assert(conversations[-1]['from'] == 'prospect')
+    assert(len(conversations) % 2 == 0)
+    for i in range(1, len(conversations)):
+        assert(conversations[i]['from'] != conversations[i-1]['from'])
+
     for c in conversations:
         length = len(tokenizer(c["value"]).input_ids) + 6
         tokenized_lens.append(length)
@@ -62,8 +74,7 @@ def split_one_sample(sample):
     start_idx = 0
     cur_len = 0
 
-    if len(conversations) % 2 != 0 or len(conversations) < 2:
-        return []
+    sample['conversations'] = conversations
 
     new_samples = []
     for i in range(0, len(conversations), 2):
@@ -87,7 +98,7 @@ def worker(input_data):
     return result
 
 
-def split_all(content, begin, end, tokenizer_, max_length_):
+def split_all(content, tokenizer_, max_length_):
     """
     Keep the maximum round of conversations within the max token length constraint
     """
@@ -112,19 +123,22 @@ def split_all(content, begin, end, tokenizer_, max_length_):
 def filter_invalid_roles(content):
     new_content = []
     for i, c in enumerate(content):
-        # roles = ["human", "gpt"]
         roles = ["sales rep", "prospect"]
-        if len(c["conversations"]) <= 0:
+        if len(c["conversations"]) <= 1:
+            print("SHORT EXAMPLE: ", len(c["conversations"]))
             continue
 
+        assert(c["conversations"][0]['from'] == 'sales rep')
+        assert(c["conversations"][-1]['from'] == 'prospect')
+        assert(len(c["conversations"]) % 2 == 0)
         valid = True
         for j, s in enumerate(c["conversations"]):
             if s["from"] != roles[j % 2]:
                 valid = False
                 break
 
-        if valid:
-            new_content.append(c)
+        assert(valid)
+        new_content.append(c)
 
     return new_content
 
@@ -137,8 +151,8 @@ def main():
         padding_side="right",
         use_fast=False,
     )
-    new_content = split_all(content, args.begin, args.end, tokenizer, args.max_length)
-    # new_content = filter_invalid_roles(new_content)
+    new_content = split_all(content, tokenizer, args.max_length)
+    new_content = filter_invalid_roles(new_content)
 
     print(f"#in: {len(content)}, #out: {len(new_content)}")
     json.dump(new_content, open(args.out_file, "w"), indent=2, ensure_ascii=False)
